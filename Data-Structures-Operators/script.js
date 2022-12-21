d959da20eda8c0967d153c65ed6525f2c580dcc6
Kirill Golubjov <kirillcnz@gmail.com>
2022-12-21 10:42:12 +0200

    Optional Chaining (?.)

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -49,6 +49,38 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+// ////////////////////////////////////////////////////////////////
+//Optional Chaining (?.)
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.() ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: `Kirill`, email: `kirillcnz@gmail.com` }];
+// const users = [];
+console.log(users[0]?.name ?? 'User array empty');
+
+// Alternative to previous one
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
 
 // ////////////////////////////////////////////////////////////////
 // //Looping Arrays: The for-of Loop

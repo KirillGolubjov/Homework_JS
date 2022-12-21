ded75ab1e71327ed914a8f2319cae7d6c3285171
Kirill Golubjov <kirillcnz@gmail.com>
2022-12-21 18:07:50 +0200

    Maps: Iteration

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -50,37 +50,72 @@ const restaurant = {
   },
 };
 ////////////////////////////////////////////////////
+// // MAPS iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Python'],
+  [3, 'Javascript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+//Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+
+//Convert map to array
+console.log([...question]);
+// console.log([question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+////////////////////////////////////////////////////
 // // MAPS
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Tallinn, Estonia'));
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Tallinn, Estonia'));
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open!')
-  .set(false, 'We are closed!');
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open!')
+//   .set(false, 'We are closed!');
 
-// console.log(rest.get('name'));
-// console.log(rest.get(true));
-// console.log(rest.get(1));
+// // console.log(rest.get('name'));
+// // console.log(rest.get(true));
+// // console.log(rest.get(1));
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('closed')));
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('closed')));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-// rest.clear();
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear();
 
-const arr = [1, 2];
-rest.set(arr, 'Test');
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-console.log(rest.size);
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
 
-console.log(rest.get(arr));
+// console.log(rest.get(arr));
 
 //////////////////////////////////
 // //SETS

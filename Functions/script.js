ee1c45258f6e82f1c680ea8968ee23961b95f6d1
Kirill Golubjov <kirillcnz@gmail.com>
2022-12-24 14:01:11 +0200

    How Passing Arguments Works: Value vs. Reference

--- Functions/script.js
+++ Functions/script.js
@@ -1,27 +1,63 @@
 'use strict';
 
-const bookings = [];
+/////////////////////////////////////////////////////////////
+// // Default Parameters
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  //  // S5
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   //  // S5
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('AH350');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 3);
+// createBooking('LH123', 4);
+// createBooking('LH123', undefined, 1000);
+
+/////////////////////////////////////////////////////////////
+// // How Passing Arguments Works: Value vs. Reference
+const flight = 'LH234';
+const kirill = {
+  name: 'Kirill Golubjov',
+  passport: 39004132236,
 };
 
-createBooking('AH350');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 3);
-createBooking('LH123', 4);
-createBooking('LH123', undefined, 1000);
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH199';
+  passenger.name = 'MR.' + passenger.name;
+
+  if (passenger.passport === 39004132236) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, kirill);
+// console.log(flight);
+// console.log(kirill);
+
+// // Is the same as doing...
+// const flightNum = flight;
+// const passenger = kirill;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(kirill);
+checkIn(flight, kirill);

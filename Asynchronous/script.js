916f3f37ada29d327ea99ad226d15113683b4f3f
Kirill Golubjov <kirillcnz@gmail.com>
2023-01-06 16:48:41 +0200

    Chaining Promises

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -101,9 +101,21 @@ const renderCountry = function (data, className = '') {
 // };
 
 const getCountyData = function (country) {
+  // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data?.[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[1];
+
+      if (!neighbour) return;
+
+      // Country 2
+
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountyData('estonia');

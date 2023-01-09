9249071a54321c9b3d3d50fdea532b23b4bce506
Kirill Golubjov <kirillcnz@gmail.com>
2023-01-09 13:34:03 +0200

    coding_challenge_#3

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -471,40 +471,125 @@ GOOD LUCK 😀
 
 // get3Countries('Estonia', 'Russia', 'Spain');
 
-// Promise.race
-(async function () {
-  const res = await Promise.race([
-    getJSON(`https://restcountries.com/v2/name/mexico`),
-    getJSON(`https://restcountries.com/v2/name/argentina`),
-    getJSON(`https://restcountries.com/v2/name/brasil`),
-  ]);
-  console.log(res[0]);
-})();
+// // Promise.race
+// (async function () {
+//   const res = await Promise.race([
+//     getJSON(`https://restcountries.com/v2/name/mexico`),
+//     getJSON(`https://restcountries.com/v2/name/argentina`),
+//     getJSON(`https://restcountries.com/v2/name/brasil`),
+//   ]);
+//   console.log(res[0]);
+// })();
 
-const timeout = function (sec) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error('Request took too long!'));
-    }, sec * 1000);
+// const timeout = function (sec) {
+//   return new Promise(function (_, reject) {
+//     setTimeout(function () {
+//       reject(new Error('Request took too long!'));
+//     }, sec * 1000);
+//   });
+// };
+
+// Promise.race(getJSON(`https://restcountries.com/v2/name/tanzania`), timeout(2))
+//   .then(res => console.log(res[0]))
+//   .catch(err => console.error(err));
+
+// // Promise.allSettled
+// Promise.allSettled([
+//   Promise.resolve('Success'),
+//   Promise.reject('ERROR'),
+//   Promise.resolve('Another success'),
+// ]).then(res => console.log(res));
+
+// // //Promise.any[ES2021]; Rejected Promises are ignored
+// Promise.any([
+//   Promise.resolve('Success'),
+//   Promise.reject('ERROR'),
+//   Promise.resolve('ANother success'),
+// ])
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err));
+
+//////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
   });
 };
 
-Promise.race(getJSON(`https://restcountries.com/v2/name/tanzania`), timeout(2))
-  .then(res => console.log(res[0]))
-  .catch(err => console.error(err));
+const imgContainer = document.querySelector('.images');
 
-// Promise.allSettled
-Promise.allSettled([
-  Promise.resolve('Success'),
-  Promise.reject('ERROR'),
-  Promise.resolve('Another success'),
-]).then(res => console.log(res));
+console.log(imgContainer);
 
-// //Promise.any[ES2021]; Rejected Promises are ignored
-Promise.any([
-  Promise.resolve('Success'),
-  Promise.reject('ERROR'),
-  Promise.resolve('ANother success'),
-])
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    console.log('Img is loading ...🔄 ');
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error(`Image not found 🚫`));
+    });
+  });
+};
+
+// const loadNPause = async function () {
+//   try {
+//     // Load image 1
+//     let img = await createImage(`img/img-1.jpg`);
+//     console.log(`Image 1 is loaded`);
+//     await wait(2);
+//     img.style.display = 'none';
+//     // Load image 2
+//     img = await createImage(`img/img-2.jpg`);
+//     console.log(`Image 2 is loaded`);
+//     await wait(2);
+//     img.style.display = 'none';
+//     // Load image 3
+//     img = await createImage(`img/img-3.jpg`);
+//     console.log(`Image 3 is loaded`);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// loadNPause();
+
+const imgArr = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'];
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = await imgArr.map(createImage);
+    console.log(imgs);
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach(el => el.classList.add('parallel'));
+    console.log(imgContainer);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+loadAll(imgArr);
